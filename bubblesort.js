c943bf2e528c5833c483d173945462e28b43e593
r0D7ynki <123518918+r0D7ynki@users.noreply.github.com>
2023-02-01 11:50:27 +0100

    Create

---
+++ bubblesort.js
@@ -0,0 +1,19 @@
+function bubbleSort(arr) {
+  let n = arr.length;
+  for (let i = 0; i < n - 1; i++) {
+    for (let j = 0; j < n - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+}
+
+//example
+//bubbleSort([4, 6, 23, 6, 4, 37, 3]);
+
+
+
